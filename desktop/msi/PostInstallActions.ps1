c299aeceacd5066ae0258acfa3922103dcb4ad5f
Petro <Xavrais312@gmail.com>
2023-01-19 16:58:19 +0200

    post inst action

--- desktop/msi/PostInstallActions.ps1
+++ desktop/msi/PostInstallActions.ps1
@@ -5,27 +5,33 @@ param
 
 Push-Location $PSScriptRoot
 
-# $fileName = "FileToInstall.txt"
-# $dateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
+$fileName = "FileToInstall.txt"
+$dateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
 
-# if(Test-Path $fileName)
-# {
-#     Add-Content -Path $fileName -Value "$dateTime - $propertyValue"
-# }
-# else
-# {
-#     throw "File $fileName does not exist"
-# }
+if (-not (Test-Path $fileName))
+{
+  throw "File $fileName does not exist"
+}
+Add-Content -Path $fileName -Value "`n$dateTime - start"
+
 
 Unregister-ScheduledTask -TaskName "CheckRemoteUpdate" -Confirm:$false -ErrorAction SilentlyContinue
+Add-Content -Path $fileName -Value "`n$dateTime Unregister-ScheduledTask"
 
-$scriptPath = Join-Path "." TaskAction.ps1 -Resolve
+# $scriptPath = Join-Path "." TaskAction.ps1 -Resolve
+# Add-Content -Path $fileName -Value "`n$dateTime - scriptPath - [$scriptPath]"
 
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' `
-  -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy ByPass `"$scriptPath`""
+# $action = New-ScheduledTaskAction -Execute 'Powershell.exe' `
+#   -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy ByPass `"$scriptPath`""
+# Add-Content -Path $fileName -Value "`n$dateTime - action - [$action]"
 
-$trigger =  New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes 1) -At 0am
+# $trigger =  New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes 1) -At 0am
+# Add-Content -Path $fileName -Value "`n$dateTime - trigger - [$trigger]"
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "CheckRemoteUpdate" -Description "Periodically check remote update"
+# $task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "CheckRemoteUpdate" -Description "Periodically check remote update"
+schtasks.exe /Create /SC minute /mo 2 /TN "CheckRemoteUpdate" /TR "C:\Program Files\CheckRemoteUpdate\PostInstallActions.ps1"
+Add-Content -Path $fileName -Value "`n$dateTime Register-ScheduledTask - [$task]"
 
 Pop-Location
+
+Add-Content -Path $fileName -Value "`n$dateTime - finish"
