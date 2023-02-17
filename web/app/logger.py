800030a4ad438ebdf61d737cbecbede12a526322
Petro <Xavrais312@gmail.com>
2023-02-17 13:45:27 +0200

    log to a file

--- web/app/logger.py
+++ web/app/logger.py
@@ -1,21 +1,16 @@
-import os
-import sys
+from pathlib import Path
 from loguru import logger
 
-LOG_FILE = os.environ.get("LOG_FILE")
 
 log_format = "{time} - {name} - {level} - {message}"
-# /var/log/project_name/LOG_FILE
-# Change LOG_FILE in .flaskenv to enable or disable (empty) logging to a file
-if LOG_FILE:
-    logger.add(
-        LOG_FILE,
-        format=log_format,
-        serialize=True,
-        level="DEBUG",
-        rotation="1 week",
-        compression="zip",
-        colorize=True
-    )
+home_log = Path.home() / "emar_server_logs.txt"
 
-logger.add(sys.stdout, format=log_format, serialize=True, level="DEBUG", colorize=True)
+logger.add(
+    Path.home(),
+    format=log_format,
+    serialize=True,
+    level="DEBUG",
+    colorize=True,
+    rotation="1000 MB",
+    compression="zip"
+)
