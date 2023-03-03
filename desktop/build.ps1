3a1cc766bf0f78c265f2c50723d0b8150691611d
kipod <nchernov@gmail.com>
2023-03-03 15:28:28 +0100

    fix: build

--- desktop/build.ps1
+++ desktop/build.ps1
@@ -1,7 +1,17 @@
+Remove-Item .\msi\dist -Recurse
+
 pyinstaller --noconfirm --onefile --console `
---distpath "." `
+--distpath "msi/dist" `
 --collect-all "paramiko" `
 --collect-all "requests" `
 --collect-all "loguru" `
 --collect-all "win32com" `
 server_connect.py
+
+pyinstaller --noconfirm --onefile --console `
+--distpath "msi/dist" `
+--collect-all "requests" `
+--collect-all "loguru" `
+heartbeat.py
+
+copy .\config.json .\msi\dist\
