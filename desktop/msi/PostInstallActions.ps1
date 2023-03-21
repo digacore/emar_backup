446cc85d62b1a742aa5b0790231fa0635cca9165
Petro <Xavrais312@gmail.com>
2023-03-21 12:45:32 +0200

    change scheduled task description

--- desktop/msi/PostInstallActions.ps1
+++ desktop/msi/PostInstallActions.ps1
@@ -147,7 +147,7 @@ Add-Content -Path $logFile -Value "`n$(Get-Date -Format `"yyyy-MM-dd HH:mm:ss K`
 $trigger = New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Hours 1) -At 0am
 Add-Content -Path $logFile -Value "`n$(Get-Date -Format `"yyyy-MM-dd HH:mm:ss K`") - trigger - [$trigger]"
 
-$task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "CheckRemoteUpdate" -Description "Periodically check remote update" -User $username -Password $password
+$task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "CheckRemoteUpdate" -Description "Periodically check remote sftp and update backups" -User $username -Password $password
 Add-Content -Path $logFile -Value "`n$(Get-Date -Format `"yyyy-MM-dd HH:mm:ss K`") Register-ScheduledTask - [$task]"
 
 
