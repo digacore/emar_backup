70fa9be9e6d4649791d25fd8ae6b5832fd514e54
Petro <Xavrais312@gmail.com>
2023-04-24 22:33:13 +0300

    alert users per location

--- web/app/controllers/alert.py
+++ web/app/controllers/alert.py
@@ -63,6 +63,7 @@ def get_html_body(computer: m.Computer, alert_obj: m.Alert):
     return html_template
 
 
+# TODO remove this func if no use in future
 def alert_additional_users(computer: m.Computer, alert_obj: m.Alert):
     # get users associated with this computer
     users: List[m.User] = m.User.query.filter(
@@ -83,7 +84,7 @@ def alert_additional_users(computer: m.Computer, alert_obj: m.Alert):
             alert_obj.name,
         )
 
-        to_addresses = user.email
+        to_addresses = [user.email]
         html_body = get_html_body(computer, alert_obj)
         requests.post(
             CFG.MAIL_ALERTS,
@@ -100,7 +101,11 @@ def alert_additional_users(computer: m.Computer, alert_obj: m.Alert):
 
 
 def check_computer_send_mail(
-    last_time: datetime, computer: m.Computer, alert_type: str, alert_obj
+    last_time: datetime,
+    computer: m.Computer,
+    alert_type: str,
+    alert_obj: m.Alert,
+    alerted_target=None,
 ):
     """Send email to support if last time online/download > alert_hours.
     If not - make status green
@@ -119,88 +124,71 @@ def check_computer_send_mail(
     alerts_time = timedelta(seconds=60 * 60 * alert_hours)
 
     # TODO find more elegant way to handle all cases
-    # convert from str to datetime
-    last_online = (
-        datetime.strptime(computer.last_time_online, TIME_FORMAT)
-        if isinstance(computer.last_time_online, str)
-        else computer.last_time_online
-    )
-    last_download = (
-        datetime.strptime(computer.last_download_time, TIME_FORMAT)
-        if isinstance(computer.last_download_time, str)
-        else computer.last_download_time
-    )
+
     # if None - consider it as time was missed to keep status red
     last_online = (
-        last_online
-        if last_online
-        else CFG.offset_to_est(datetime.now(), True) - alerts_time * 2
+        computer.last_time_online
+        if computer
+        else CFG.offset_to_est(datetime.utcnow(), True) - alerts_time * 2
     )
     last_download = (
-        last_download
-        if last_download
-        else CFG.offset_to_est(datetime.now(), True) - alerts_time * 2
+        computer.last_download_time
+        if computer
+        else CFG.offset_to_est(datetime.utcnow(), True) - alerts_time * 2
+    )
+    # convert from str to datetime
+    last_online = (
+        datetime.strptime(last_online, TIME_FORMAT)
+        if isinstance(last_online, str)
+        else last_online
+    )
+    last_download = (
+        datetime.strptime(last_download, TIME_FORMAT)
+        if isinstance(last_download, str)
+        else last_download
     )
 
-    if not last_time and not computer:
+    if not last_time and not computer and alerted_target:
+        to_addresses: list = m.User.query.filter_by(asociated_with=alerted_target).all()
+        body = alert_obj.body if alert_obj.body else ""
+        html_body = alert_obj.html_body if alert_obj.html_body else ""
+
+        # TODO temporary to_addresses.extend(). Deside if to send mail to Global users
+
         requests.post(
             alert_url,
             json={
-                "alerted_target": "all",
+                "alerted_target": alerted_target,
                 "alert_status": alert_obj.alert_status,
                 "from_email": alert_obj.from_email,
-                "to_addresses": alert_obj.to_addresses,
+                "to_addresses": to_addresses.extend(
+                    ["xavrais312@gmail.com", "nberger@digacore.com"]
+                ),
                 "subject": alert_obj.subject,
-                "body": alert_obj.body,
-                "html_body": alert_obj.html_body,
+                "body": body,
+                "html_body": html_body,
             },
         )
 
-        all_computers = m.Computer.query.all()
+        # TODO remove if overkill
+        all_computers = m.Computer.query.filter_by(location_name=alerted_target).all()
         for computer in all_computers:
             computer.alert_status = f"red - {alert_type}"
             computer.update()
         logger.warning(
-            "Computer {} {} hours {} alert sent and alert status updated to red.",
-            "ALL",
-            "null",
+            "Location {} {} alert sent and alert statuses updated to red.",
+            alerted_target,
             alert_type,
         )
 
     elif last_time < CFG.offset_to_est(datetime.now(), True) - alerts_time and (
         computer.alert_status == "green" or not computer.alert_status
     ):
-        requests.post(
-            alert_url,
-            json={
-                "alerted_target": computer.computer_name,
-                "alert_status": alert_obj.alert_status,
-                # TODO Is from_email going to be configurable or take from BCG? Most likely yes
-                "from_email": alert_obj.from_email,
-                "to_addresses": alert_obj.to_addresses,
-                "subject": f"{computer.computer_name} {alert_obj.subject}",
-                "body": f"{computer.computer_name} {alert_obj.body}",
-                "html_body": alert_obj.html_body,
-            },
-        )
-        alert_additional_users(computer, alert_obj)
 
         computer.alert_status = f"yellow - {alert_type}"
         computer.update()
         logger.warning(
-            "Computer {} {} hours {} alert sent and alert status updated to yellow.",
-            computer.computer_name,
-            alert_hours,
-            alert_type,
-        )
-    elif (
-        last_time < CFG.offset_to_est(datetime.now(), True) - alerts_time
-        and "yellow" in computer.alert_status
-    ):
-        computer.alert_status = f"red - {alert_type}"
-        computer.update()
-        logger.warning(
-            "Computer {} {} hours {} alert status updated to red due to repeated yellow condition.",
+            "Computer {} {} hours {} alert status updated to yellow.",
             computer.computer_name,
             alert_hours,
             alert_type,
@@ -226,25 +214,14 @@ def check_computer_send_mail(
         )
 
 
-def alert_status_red(computers: list[m.Computer]):
-    """Check if all current computers are red. If no - uppdate.
-    If all red we don't send email again.
-
-    Args:
-        computers (list[models.Computer]): _description_
-
-    Returns:
-        int: quantity of red computers
-    """
-
-    all_red = 0
-    for computer in computers:
-        if computer.alert_status == "red":
-            all_red += 1
-        else:
-            computer.alert_status = "red"
-            computer.update()
-    return all_red
+def time_type_check(time_obj):
+    # last_download_time str check
+    last_time = (
+        datetime.strptime(time_obj, TIME_FORMAT)
+        if isinstance(time_obj, str)
+        else time_obj
+    )
+    return last_time
 
 
 def check_and_alert():
@@ -253,104 +230,105 @@ def check_and_alert():
     Checks computers activity.
     Send email and change status if something went wrong.
     """
+    locations: list[m.Location] = m.Location.query.all()
     # TODO update query to check computer last time inside database
     computers: list[m.Computer] = m.Computer.query.all()
     # TODO loop for all CUSTOM alerts to send email
     alerts: list[m.Alert] = m.Alert.query.all()
-    alert_names = [i.name for i in alerts]
 
-    off_30_min_computers = 0
-    no_update_files_2h = 0
+    alert_names = {alert.name: alert for alert in alerts}
+    location_computers = {location.name: [] for location in locations}
 
     for computer in computers:
+        location_computers[computer.location_name].append(computer)
 
-        alert_types_computers = {
-            "no_download_12h": computer.last_download_time,
-            "offline_12h": computer.last_time_online,
-        }
+    for location in location_computers:
+        off_30_min_computers = 0
+        no_update_files_2h = 0
 
-        # last_download_time str check
-        last_download_time = (
-            datetime.strptime(computer.last_download_time, TIME_FORMAT)
-            if isinstance(computer.last_download_time, str)
-            else computer.last_download_time
-        )
+        for computer in location_computers[location]:
 
-        # last_time_online str check
-        last_time_online = (
-            datetime.strptime(computer.last_time_online, TIME_FORMAT)
-            if isinstance(computer.last_time_online, str)
-            else computer.last_time_online
-        )
+            alert_types_computers = {
+                "no_download_12h": computer.last_download_time,
+                "offline_12h": computer.last_time_online,
+            }
 
-        # check alert_types and computers. Send email if computer outdated
-        for alert_type in alert_types_computers:
-            if alert_types_computers[alert_type] and alert_type in alert_names:
-                alert_obj: m.Alert = m.Alert.query.filter_by(name=alert_type).first()
+            last_download_time = time_type_check(computer.last_download_time)
+            last_time_online = time_type_check(computer.last_time_online)
 
-                # TODO find more elegant way to handle all cases
-                if last_time_online and alert_type == "offline_12h":
-                    if last_time_online < CFG.offset_to_est(
-                        datetime.now(), True
-                    ) - timedelta(seconds=1800):
-                        off_30_min_computers += 1
+            # check alert_types and computers. Send email if computer outdated
+            for alert_type in alert_types_computers:
+                if alert_types_computers[alert_type] and alert_type in alert_names:
+                    alert_obj: m.Alert = alert_names[alert_type]
 
-                    check_computer_send_mail(
-                        last_time=last_time_online,
-                        computer=computer,
-                        alert_type=alert_type,
-                        alert_obj=alert_obj,
-                    )
+                    # TODO apply something like this to DRY
+                    # def handle_alert_case(last_time, alert_type, computer, alert_obj):
+                    #     alerts_timedelta = {"offline_12h": 1800, "no_download_12h": 7200}
 
-                if last_download_time and alert_type == "no_download_12h":
-                    if last_download_time < CFG.offset_to_est(
-                        datetime.now(), True
-                    ) - timedelta(seconds=7200):
-                        no_update_files_2h += 1
+                    #     if last_time < CFG.offset_to_est(
+                    #         datetime.now(), True
+                    #     ) - timedelta(seconds=alerts_timedelta[alert_type]):
 
-                    check_computer_send_mail(
-                        last_time=last_download_time,
-                        computer=computer,
-                        alert_type=alert_type,
-                        alert_obj=alert_obj,
-                    )
+                    #         check_computer_send_mail(
+                    #         last_time=last_time,
+                    #         computer=computer,
+                    #         alert_type=alert_type,
+                    #         alert_obj=alert_obj,
+                    #     )
+                    #         return 1
 
-    if off_30_min_computers == len(computers):
-        logger.warning("All computers offline 30 min alert.")
+                    # TODO find more elegant way to handle all cases
+                    if last_time_online and alert_type == "offline_12h":
+                        if last_time_online < CFG.offset_to_est(
+                            datetime.now(), True
+                        ) - timedelta(seconds=1800):
+                            off_30_min_computers += 1
 
-        if alert_status_red(computers) != len(computers):
-            all_offline: m.Alert = (
-                m.Alert.query.filter_by(name="all_offline").first()
-                if "all_offline" in alert_names
-                else None
-            )
+                        check_computer_send_mail(
+                            last_time=last_time_online,
+                            computer=computer,
+                            alert_type=alert_type,
+                            alert_obj=alert_obj,
+                        )
+
+                    if last_download_time and alert_type == "no_download_12h":
+                        if last_download_time < CFG.offset_to_est(
+                            datetime.now(), True
+                        ) - timedelta(seconds=7200):
+                            no_update_files_2h += 1
+
+                        check_computer_send_mail(
+                            last_time=last_download_time,
+                            computer=computer,
+                            alert_type=alert_type,
+                            alert_obj=alert_obj,
+                        )
+
+        # check if all computer are yellow in every location. If yes - alert
+        if off_30_min_computers == len(location_computers[location]):
+
             check_computer_send_mail(
                 last_time=None,
                 computer=None,
                 alert_type="all offline 30 min",
-                alert_obj=all_offline,
+                alert_obj=alert_names["all_offline"],
+                alerted_target=location,
             )
             logger.warning(
-                "All computers offline 30 min alert sent and alert statuses updated."
+                "All computers from location {} offline 30 min alert.", location
             )
 
-    if no_update_files_2h == len(computers):
-        logger.warning("No new files over 2 h alert.")
+        if no_update_files_2h == len(location_computers[location]):
 
-        if alert_status_red(computers) != len(computers):
-            no_files_2h: m.Alert = (
-                m.Alert.query.filter_by(name="no_files_2h").first()
-                if "no_files_2h" in alert_names
-                else None
-            )
             check_computer_send_mail(
                 last_time=None,
                 computer=None,
                 alert_type="no new files 2 h",
-                alert_obj=no_files_2h,
+                alert_obj=alert_names["no_files_2h"],
+                alerted_target=location,
             )
 
-            logger.info("No new files over 2 h alert sent and alert statuses updated.")
+            logger.warning("No new files over 2 h alert in location {}.", location)
 
 
 def daily_summary():
@@ -510,7 +488,7 @@ def daily_summary():
             json={
                 "alerted_target": "daily_summary",
                 "alert_status": "daily_summary",
-                "to_addresses": recipient,
+                "to_addresses": [recipient],
                 "subject": f"eMARVault daily summary for {email_user[recipient].asociated_with}",
                 "body": "",
                 "html_body": html_template,
