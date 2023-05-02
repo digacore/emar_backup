b288280a3975cbaba999c51cf7ce1793d44ae849
Petro <Xavrais312@gmail.com>
2023-05-02 12:20:28 +0300

    new cards for every user

--- web/app/views/main.py
+++ web/app/views/main.py
@@ -20,105 +20,14 @@ def index():
         total_locations: list[Location] = Location.query.all()
         total_computers: list[Computer] = Computer.query.all()
 
-        # count locations_offline
-        locations_status = {loc.name: [] for loc in total_locations}
-        # add computers to appropriate location
-        for comp in total_computers:
-            if comp.location_name:
-                locations_status[comp.location_name].append(comp.alert_status)
-
-        # find out in which location all computers are offline
-        for loc in locations_status:
-            loc_comps_stats = locations_status[loc]  # computers in location
-            loc_status_count = 0
-            for comp_stat in loc_comps_stats:
-                if "offline" in comp_stat:
-                    loc_status_count += 1
-            if loc_status_count == len(loc_comps_stats) and len(loc_comps_stats) != 0:
-                locations_status[loc] = "offline"
-            else:
-                locations_status[loc] = "online"
-
-        locations_offline = [
-            loc for loc in locations_status if locations_status[loc] == "offline"
-        ]
-
-        locations_offline_perc = get_percentage(total_locations, locations_offline)
-
-        # count locations_no_download
-        locations_d_status = {loc.name: [] for loc in total_locations}
-        # add computers to appropriate location
-        for comp in total_computers:
-            if comp.location_name:
-                locations_d_status[comp.location_name].append(comp.alert_status)
-
-        # find out in which location allcomputers are no download
-        for loc in locations_d_status:
-            loc_comps_stats = locations_d_status[loc]  # computers in location
-            loc_status_count = 0
-            for comp_stat in loc_comps_stats:
-                if "no backup" in comp_stat:
-                    loc_status_count += 1
-            if loc_status_count == len(loc_comps_stats) and len(loc_comps_stats) != 0:
-                locations_d_status[loc] = "no backup"
-            else:
-                locations_d_status[loc] = "backup"
-
-        locations_no_download = [
-            loc for loc in locations_d_status if locations_d_status[loc] == "no backup"
-        ]
-
-        locations_no_download_perc = get_percentage(
-            total_locations, locations_no_download
-        )
-
-        offline_48h = get_outdated_status_comps(total_computers, 48, "offline")
-        offline_48h_perc = get_percentage(total_computers, offline_48h)
-
-        no_backup_4h = get_outdated_status_comps(total_computers, 4, "backup")
-        no_backup_4h_perc = get_percentage(total_computers, no_backup_4h)
-
-        stable_msi: DesktopClient = DesktopClient.query.filter_by(
-            flag_name="stable"
-        ).first()
-
         # TODO find out which computers are required here: NOT stable/latest OR status Red???
         # current_version_date_comp = len(
         #     Computer.query.filter(
         #         or_(Computer.msi_version == "stable", Computer.msi_version == "latest")
         #     ).all()
         # )
-        current_version_date_comp = Computer.query.filter(
-            Computer.alert_status == "red"
-        ).all()
+        total_computers_query = Computer.query.filter(Computer.alert_status == "red")
 
-        # create a link for Locations filtering for red cards (danger) at index.html
-        list_filter_str = "?flt0_12="
-        alerted_companies_offline = "percen2C".join(
-            list(
-                set(
-                    [
-                        comp.company_name.replace(" ", "+")
-                        for comp in offline_48h
-                        if comp.company_name
-                    ]
-                )
-            )
-        )
-        alerted_companies_no_backup = "percen2C".join(
-            list(
-                set(
-                    [
-                        comp.company_name.replace(" ", "+")
-                        for comp in no_backup_4h
-                        if comp.company_name
-                    ]
-                )
-            )
-        )
-
-        al_com_off_filter_link = f"{list_filter_str}{alerted_companies_offline}"
-        al_com_no_back_filter_link = f"{list_filter_str}{alerted_companies_no_backup}"
     else:
         total_companies = Company.query.filter_by(name=viewer.asociated_with).all()
         total_locations = (
@@ -144,14 +53,103 @@ def index():
         #         )
         #     ).all()
         # )
-        current_version_date_comp = total_computers_query.filter(
-            Computer.alert_status == "red",
-        ).all()
+
+    current_version_date_comp = total_computers_query.filter(
+        Computer.alert_status == "red",
+    ).all()
 
     # NOTE use if NOT stable/latest computers are required
     # out_of_date_comp = total_computers - current_version_date_comp
     out_of_date_comp = len(current_version_date_comp)
     out_of_date_comp_perc = get_percentage(total_computers, current_version_date_comp)
+
+    # count locations_offline
+    locations_status = {loc.name: [] for loc in total_locations}
+    # add computers to appropriate location
+    for comp in total_computers:
+        if comp.location_name:
+            locations_status[comp.location_name].append(comp.alert_status)
+
+    # find out in which location all computers are offline
+    for loc in locations_status:
+        loc_comps_stats = locations_status[loc]  # computers in location
+        loc_status_count = 0
+        for comp_stat in loc_comps_stats:
+            if "offline" in comp_stat:
+                loc_status_count += 1
+        if loc_status_count == len(loc_comps_stats) and len(loc_comps_stats) != 0:
+            locations_status[loc] = "offline"
+        else:
+            locations_status[loc] = "online"
+
+    locations_offline = [
+        loc for loc in locations_status if locations_status[loc] == "offline"
+    ]
+
+    locations_offline_perc = get_percentage(total_locations, locations_offline)
+
+    # count locations_no_download
+    locations_d_status = {loc.name: [] for loc in total_locations}
+    # add computers to appropriate location
+    for comp in total_computers:
+        if comp.location_name:
+            locations_d_status[comp.location_name].append(comp.alert_status)
+
+    # find out in which location allcomputers are no download
+    for loc in locations_d_status:
+        loc_comps_stats = locations_d_status[loc]  # computers in location
+        loc_status_count = 0
+        for comp_stat in loc_comps_stats:
+            if "no backup" in comp_stat:
+                loc_status_count += 1
+        if loc_status_count == len(loc_comps_stats) and len(loc_comps_stats) != 0:
+            locations_d_status[loc] = "no backup"
+        else:
+            locations_d_status[loc] = "backup"
+
+    locations_no_download = [
+        loc for loc in locations_d_status if locations_d_status[loc] == "no backup"
+    ]
+
+    locations_no_download_perc = get_percentage(total_locations, locations_no_download)
+
+    offline_48h = get_outdated_status_comps(total_computers, 48, "offline")
+    offline_48h_perc = get_percentage(total_computers, offline_48h)
+
+    no_backup_4h = get_outdated_status_comps(total_computers, 4, "backup")
+    no_backup_4h_perc = get_percentage(total_computers, no_backup_4h)
+
+    stable_msi: DesktopClient = DesktopClient.query.filter_by(
+        flag_name="stable"
+    ).first()
+
+    # create a link for Locations filtering for red cards (danger) at index.html
+    list_filter_str = "?flt0_12="
+    alerted_companies_offline = "percen2C".join(
+        list(
+            set(
+                [
+                    comp.company_name.replace(" ", "+")
+                    for comp in offline_48h
+                    if comp.company_name
+                ]
+            )
+        )
+    )
+    alerted_companies_no_backup = "percen2C".join(
+        list(
+            set(
+                [
+                    comp.company_name.replace(" ", "+")
+                    for comp in no_backup_4h
+                    if comp.company_name
+                ]
+            )
+        )
+    )
+
+    al_com_off_filter_link = f"{list_filter_str}{alerted_companies_offline}"
+    al_com_no_back_filter_link = f"{list_filter_str}{alerted_companies_no_backup}"
 
     return render_template(
         "index.html",
