63ff71387be35b40cf45b704a9d0e58ebf80b304
Petro <Xavrais312@gmail.com>
2023-05-15 17:43:14 +0300

    clean auth

--- web/app/views/auth.py
+++ web/app/views/auth.py
@@ -119,14 +119,10 @@ def callback():
     # Create a user in our db with the information provided
     # by Google
     if not user:
-        # TODO temporary setup. Change activated to False and asociated_with to empty
         user = User(
             username=f"{users_name}-{unique_id}",
             email=users_email,
-            # password=CFG.SSO_DEF_PASSWORD,
             password=uuid4(),
-            # activated=True,
-            # asociated_with="Test-Company",
         )
         user.save()
     if not user.activated:
@@ -187,14 +183,10 @@ def auth_response():
 
     # Create a user in our db with the information provided by Microsoft
     if not user:
-        # TODO temporary setup. Change activated to False and asociated_with to empty
         user = User(
             username=str(result["name"]).lower().replace(" ", "-"),
             email=result["preferred_username"],
-            # password=CFG.SSO_DEF_PASSWORD,
             password=uuid4(),
-            # activated=True,
-            # asociated_with="Test-Company",
         )
         user.save()
     if not user.activated:
@@ -207,134 +199,3 @@ def auth_response():
     # Send user back to homepage
     flash("SSO login successful.", "success")
     return redirect(url_for("main.index"))
-
-
-# NOTE this could be useful for APIs
-
-# @auth_blueprint.route("/call_downstream_api")
-# def call_downstream_api():
-#     token = auth.get_token_for_user(CFG.MICRO_SCOPE)
-#     if "error" in token:
-#         return redirect(url_for("auth.login"))
-#     # Use access token to call downstream api
-#     api_result = requests.get(
-#         CFG.MICRO_ENDPOINT,
-#         headers={"Authorization": "Bearer " + token["access_token"]},
-#         timeout=30,
-#     ).json()
-#     return render_template("index.html", result=api_result)
-
-
-# NOTE for API directly with token usage
-
-# from flask import session
-# import google.oauth2.credentials
-# import google_auth_oauthlib.flow
-
-# # This variable specifies the name of a file that contains the OAuth 2.0
-# # information for this application, including its client_id and client_secret.
-# CLIENT_SECRETS_FILE = "client_secret_556.json"
-
-# # This OAuth 2.0 access scope allows for full read/write access to the
-# # authenticated user's account and requires requests to use an SSL connection.
-# SCOPES = [
-#     "https://www.googleapis.com/auth/drive.metadata.readonly",
-#     "openid",
-#     "https://www.googleapis.com/auth/userinfo.email",
-#     "https://www.googleapis.com/auth/userinfo.profile",
-# ]
-# API_SERVICE_NAME = "drive"
-# API_VERSION = "v2"
-
-# @auth_blueprint.route("/gauthorize")
-# def gauthorize():
-#     # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-#     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-#         CLIENT_SECRETS_FILE, scopes=SCOPES
-#     )
-
-#     # The URI created here must exactly match one of the authorized redirect URIs
-#     # for the OAuth 2.0 client, which you configured in the API Console. If this
-#     # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
-#     # error.
-#     flow.redirect_uri = url_for("auth.oauth2callback", _external=True)
-#     logger.debug("flow.redirect_uri {}", flow.redirect_uri)
-
-#     authorization_url, state = flow.authorization_url(
-#         # Enable offline access so that you can refresh an access token without
-#         # re-prompting the user for permission. Recommended for web server apps.
-#         access_type="offline",
-#         # Enable incremental authorization. Recommended as a best practice.
-#         include_granted_scopes="true",
-#     )
-
-#     # Store the state so the callback can verify the auth server response.
-#     session["state"] = state
-#     logger.debug("authorization_url {}", authorization_url)
-
-#     return redirect(authorization_url)
-
-
-# @auth_blueprint.route("/oauth2callback")
-# def oauth2callback():
-#     # Specify the state when creating the flow in the callback so that it can
-#     # verified in the authorization server response.
-#     state = session["state"]
-
-#     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-#         CLIENT_SECRETS_FILE, scopes=SCOPES, state=state
-#     )
-#     flow.redirect_uri = url_for("auth.oauth2callback", _external=True)
-
-#     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-#     authorization_response = request.url
-#     flow.fetch_token(authorization_response=authorization_response)
-
-#     # Store credentials in the session.
-#     # ACTION ITEM: In a production app, you likely want to save these
-#     #              credentials in a persistent database instead.
-#     credentials = flow.credentials
-#     session["credentials"] = credentials_to_dict(credentials)
-#     flash("SSO login successful.", "success")
-#     return redirect(url_for("main.index"))
-
-
-# @auth_blueprint.route("/revoke")
-# def revoke():
-#     if "credentials" not in session:
-#         return (
-#             'You need to <a href="/gauthorize">authorize</a> before '
-#             + "testing the code to revoke credentials."
-#         )
-
-#     credentials = google.oauth2.credentials.Credentials(**session["credentials"])
-
-#     revoke = requests.post(
-#         "https://oauth2.googleapis.com/revoke",
-#         params={"token": credentials.token},
-#         headers={"content-type": "application/x-www-form-urlencoded"},
-#     )
-
-#     status_code = getattr(revoke, "status_code")
-#     if status_code == 200:
-#         return "Credentials successfully revoked." + print_index_table()
-#     else:
-#         return "An error occurred." + print_index_table()
-
-
-# @auth_blueprint.route("/clear")
-# def clear_credentials():
-#     if "credentials" in session:
-#         del session["credentials"]
-#     return "Credentials have been cleared.<br><br>" + print_index_table()
-
-
-# def credentials_to_dict(credentials):
-#     return {
-#         "token": credentials.token,
-#         "refresh_token": credentials.refresh_token,
-#         "token_uri": credentials.token_uri,
-#         "client_id": credentials.client_id,
-#         "client_secret": credentials.client_secret,
-#         "scopes": credentials.scopes,
-#     }
