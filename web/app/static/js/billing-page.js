aac07b5a65a4116a5d5d400416954c06d6995c0a
Danil Dvorchuk <dvorchyk.d@gmail.com>
2023-10-31 11:27:38 +0200

    Ready billing page

---
+++ web/app/static/js/billing-page.js
@@ -0,0 +1,25 @@
+const searchInput = document.querySelector("#company-search-input");
+if (searchInput) {
+  searchInput.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") {
+      let oldParams = (new URL(window.location)).searchParams;
+      let url = new URL(window.location.href);
+      url.searchParams.set("q", searchInput.value);
+      // reset page to 1
+      url.searchParams.set("page", "1");
+      url.searchParams.set("per_page", oldParams.get("per_page") || "25");
+      window.location.href = "".concat(url.href);
+    }
+  });
+}
+
+const clearSearchInput = (e) => {
+  searchInput.value = "";
+  let oldParams = (new URL(window.location)).searchParams;
+  let url = new URL(window.location.href);
+  url.searchParams.set("q", "");
+  // reset page to 1
+  url.searchParams.set("page", "1");
+  url.searchParams.set("per_page", oldParams.get("per_page") || "25");
+  window.location.href = "".concat(url.href);
+}
--- web/app/views/billing.py
+++ web/app/views/billing.py
@@ -1,7 +1,8 @@
-from flask import render_template, Blueprint, abort
+from flask import render_template, Blueprint, abort, request
 from flask_login import login_required, current_user
 
-from app import models as m
+from app import models as m, db
+from app.controllers import create_pagination
 
 
 billing_blueprint = Blueprint("billing", __name__, url_prefix="/billing")
@@ -14,10 +15,31 @@ def get_billing_page():
     if current_user.permission != m.UserPermissionLevel.GLOBAL:
         abort(403, "You don't have permission to access this page.")
 
-    # Retrieve all companies
-    companies = m.Company.query.filter(m.Company.is_global.is_(False)).all()
+    q = request.args.get("q", type=str, default=None)
+    per_page = request.args.get("per_page", 25, type=int)
+
+    # Companies query
+    companies_query = m.Company.query.filter(m.Company.is_global.is_(False))
+
+    # Filter query by search query
+    if q:
+        companies_query = companies_query.filter((m.Company.name.ilike(f"%{q}%")))
+
+    # Pagination
+    pagination = create_pagination(total=m.count(companies_query), page_size=per_page)
+
+    companies = (
+        db.session.execute(
+            companies_query.order_by(m.Company.name)
+            .limit(pagination.per_page)
+            .offset(pagination.skip)
+        )
+        .scalars()
+        .all()
+    )
 
     return render_template(
         "billing-page.html",
+        page=pagination,
         companies=companies,
     )
