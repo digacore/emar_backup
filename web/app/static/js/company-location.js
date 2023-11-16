45f5cad9360bd8b88a73ef4e7e4d5d7f6125dcc5
Danil Dvorchuk <dvorchyk.d@gmail.com>
2023-11-16 23:14:12 +0200

    Base for actions selecting on the Company/Location tables

--- web/app/static/js/company-location.js
+++ web/app/static/js/company-location.js
@@ -129,3 +129,34 @@ searchObjectInputs.forEach((searchObjectInput) => {
     selectSecondaryMergeObject(objectHintsContainer);
   });
 });
+
+const selectedActionsWrapper = document.querySelector(".selected-actions-wrapper");
+const itemCheckboxes = document.querySelectorAll(".action-checkbox");
+itemCheckboxes.forEach((itemCheckbox) => {
+  itemCheckbox.addEventListener("change", async (e) => {
+    const actionsWrapper = document.querySelector(`.actions-wrapper-${e.target.value}`);
+    if (e.currentTarget.checked) {
+      selectedActionsWrapper.innerHTML = null;
+
+      const actions = actionsWrapper.childNodes;
+      actions.forEach((action) => {
+        const actionClone = action.cloneNode(true);
+        if (action.className === "icon" && action.tagName === "A") {
+          actionClone.style.color = "inherit";
+        } else if (action.className === "icon" && action.tagName === "FORM") {
+          childNodes = actionClone.childNodes;
+          childNodes.forEach((childNode) => {
+            if (childNode.tagName === "BUTTON") {
+              childNode.style.border = "none";
+              childNode.style.background = "inherit";
+            }
+          });
+        }
+
+        selectedActionsWrapper.appendChild(actionClone);
+      });
+    } else {
+      selectedActionsWrapper.innerHTML = null;
+    }
+  });
+});
