c8fa6639a7fa5e2d60b514990fb2db408bf43544
Kostia <saintkos117@gmail.com>
2024-04-15 10:52:11 +0300

    fix schemas for location

--- web/app/schema/location.py
+++ web/app/schema/location.py
@@ -8,7 +8,7 @@ class LocationInfo(BaseModel):
     total_computers: int
     total_computers_offline: int
     primary_computers_offline: int
-    default_sftp_path: str
+    default_sftp_path: str | None
     pcc_fac_id: int
 
     class Config:
