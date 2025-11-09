7d1b8e927045681cb80c5967895e72846cb137af
Kostia Stoliarskiy <saintkos117@gmail.com>
2025-11-09 15:17:27 +0200

    fix comments

--- web/app/views/download_csv.py
+++ web/app/views/download_csv.py
@@ -14,6 +14,27 @@ from app.views.utils import (
     parse_search_params,
 )
 
+# Date filters
+date_mappings = [
+    ("last_time_online", m.Computer.last_time_online),
+    ("last_download_time", m.Computer.last_download_time),
+]
+
+# Join filters
+join_mappings = [
+    ("company_name", m.Company, m.Company.name),
+    ("location_name", m.Location, m.Location.name),
+]
+
+# Text filters
+text_mappings = [
+    ("computer_name", m.Computer.computer_name),
+    ("status", m.Computer.status),
+    ("device_role", m.Computer.device_role),
+    ("device_type", m.Computer.device_type),
+    ("computer_ip", m.Computer.computer_ip),
+]
+
 download_csv_blueprint = Blueprint("download_csv", __name__, url_prefix="/download_csv")
 
 
@@ -31,38 +52,17 @@ def download_csv():
 
     computers_query = m.Computer.query
 
-    # Text filters
-    text_mappings = [
-        ("computer_name", m.Computer.computer_name),
-        ("status", m.Computer.status),
-        ("device_role", m.Computer.device_role),
-        ("device_type", m.Computer.device_type),
-        ("computer_ip", m.Computer.computer_ip),
-    ]
-
     for filter_key, field in text_mappings:
         if filter_key in filters:
             computers_query = apply_text_filter(
                 computers_query, field, filters[filter_key]
             )
 
-    # Join filters
-    join_mappings = [
-        ("company_name", m.Company, m.Company.name),
-        ("location_name", m.Location, m.Location.name),
-    ]
-
     for filter_key, join_model, field in join_mappings:
         if filter_key in filters:
             computers_query = apply_join_filter(
                 computers_query, join_model, field, filters[filter_key]
             )
-
-    # Date filters
-    date_mappings = [
-        ("last_time_online", m.Computer.last_time_online),
-        ("last_download_time", m.Computer.last_download_time),
-    ]
 
     for filter_key, field in date_mappings:
         if filter_key in filters:
@@ -74,47 +74,46 @@ def download_csv():
 
     logger.info(f"Found {len(computers)} computers matching filters")
 
-    output = io.StringIO()
-    writer = csv.writer(output)
+    with io.StringIO() as output:
+        writer = csv.writer(output)
 
-    writer.writerow(
-        [
-            "Subscription",
-            "Computer Name",
-            "Status",
-            "Company Name",
-            "Location Name",
-            "Location Status",
-            "Device Type",
-            "Last time online",
-            "Last download time",
-            "Computer IP",
-        ]
-    )
-
-    for computer in computers:
         writer.writerow(
             [
-                "Pro" if not computer.is_company_trial else "Lite",
-                computer.computer_name or "",
-                computer.status or "",
-                computer.company_name or "",
-                computer.location_name if computer.location else "",
-                computer.location_status if computer.location else "",
-                computer.device_type or "",
-                computer.last_time_online.strftime("%Y-%m-%d %H:%M:%S")
-                if computer.last_time_online
-                else "",
-                computer.last_download_time.strftime("%Y-%m-%d %H:%M:%S")
-                if computer.last_download_time
-                else "",
-                computer.computer_ip or "",
+                "Subscription",
+                "Computer Name",
+                "Status",
+                "Company Name",
+                "Location Name",
+                "Location Status",
+                "Device Type",
+                "Last time online",
+                "Last download time",
+                "Computer IP",
             ]
         )
 
-    # Get CSV content
-    csv_content = output.getvalue()
-    output.close()
+        for computer in computers:
+            writer.writerow(
+                [
+                    "Pro" if not computer.is_company_trial else "Lite",
+                    computer.computer_name or "",
+                    computer.status or "",
+                    computer.company_name or "",
+                    computer.location_name if computer.location else "",
+                    computer.location_status if computer.location else "",
+                    computer.device_type or "",
+                    computer.last_time_online.strftime("%Y-%m-%d %H:%M:%S")
+                    if computer.last_time_online
+                    else "",
+                    computer.last_download_time.strftime("%Y-%m-%d %H:%M:%S")
+                    if computer.last_download_time
+                    else "",
+                    computer.computer_ip or "",
+                ]
+            )
+
+        # Get CSV content
+        csv_content = output.getvalue()
 
     # Return CSV file for download
     logger.info("CSV generation completed, sending file to user")
